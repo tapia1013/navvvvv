8d25e4bc6a2ebb0070f6ff7bd8d324c8c39a508f
tapia1013 <53988501+tapia1013@users.noreply.github.com>
2019-10-05 11:37:57 -0700

    Add files via upload

---
+++ navvvvv/main.js
@@ -0,0 +1,13 @@
+let isActive = false;
+
+$('.menu').on('click', () => {
+  if(isActive) {
+    $(this).removeClass('active');
+    $('body').removeClass('menu-open');
+  } else {
+     $(this).addClass('active');
+     $('body').addClass('menu-open');
+  }
+
+  isActive = !isActive;
+})
